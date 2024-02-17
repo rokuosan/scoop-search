1f0ebaf21fb8fb11012b5c27a0992c4006a1b5de
rokuosan <85651386+rokuosan@users.noreply.github.com>
2024-02-17 22:03:47 +0900

    Optimize dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     alias(libs.plugins.multiplatform)
-    alias(libs.plugins.buildConfig)
     alias(libs.plugins.kotlinx.serialization)
-//    alias(libs.plugins.sqlDelight)
 }
 
 group = "io.github.rokuosan"
@@ -26,42 +24,13 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation(libs.kotlinx.coroutines.core)
-                implementation(libs.kotlinx.datetime)
                 implementation(libs.ktor.core)
                 implementation(libs.ktor.client.winhttp)
                 implementation(libs.ktor.client.content.negotiation)
                 implementation(libs.ktor.serialization.kotlinx.json)
-                implementation(libs.multiplatformSettings)
                 implementation(libs.clikt)
             }
         }
-
-//        val jvmMain by getting {
-//            dependencies {
-//                implementation(libs.kotlinx.coroutines.core)
-//                implementation(libs.kotlinx.datetime)
-//                implementation(libs.ktor.core)
-//                implementation(libs.ktor.client.content.negotiation)
-//                implementation(libs.ktor.serialization.kotlinx.json)
-//                implementation(libs.multiplatformSettings)
-//                implementation(libs.ktor.client.winhttp)
-//            }
-//        }
     }
 
 }
-
-buildConfig {
-    // BuildConfig configuration here.
-    // https://github.com/gmazzo/gradle-buildconfig-plugin#usage-in-kts
-}
-
-//sqldelight {
-//    databases {
-//        create("MyDatabase") {
-//            // Database configuration here.
-//            // https://cashapp.github.io/sqldelight
-//            packageName.set("org.company.app.db")
-//        }
-//    }
-//}
